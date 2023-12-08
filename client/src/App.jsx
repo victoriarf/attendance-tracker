7dbd314d485decc3483f8b21a1344d7264f61a9b
Viktoriia <viktoriia.vasyliv@sigma.software>
2023-12-08 14:06:46 +0200

    Added MUI library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
+import {Button, Stack} from "@mui/material";
+import React from "react";
 import {useQuery} from "react-query";
 import getUsers from "./api/getUsers";
 
 function App() {
   const {isLoading, data: users} = useQuery('users', getUsers);
-    return (
+  return (
       <>
         {isLoading ? (<p className="loading"> Loading... </p>) : (
-            users.map((user) => (
-                <div key={user.name}><p> {user.name} </p></div>
-            ))
+            <Stack
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={0.5}
+            >
+              {users.map((user) => (
+                  <div key={user.name}>
+                    <Button variant='contained' size={"medium"}> {user.name}</Button>
+                  </div>
+              ))}
+            </Stack>
         )}
 
       </>
---
+++ client/src/components/User-button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Button} from "@mui/material";
+
+export const UserButton = () => {
+  return (
+      <Button variant='contained'></Button>
+  )
+}
