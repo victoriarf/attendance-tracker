15c4224969f2ba3eb619af3b927b67385941e8c8
Viktoriia <viktoriia.vasyliv@sigma.software>
2024-02-12 17:14:37 +0200

    Add dotenv.

--- server/index.js
+++ server/index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const config = require('config')
-const app = express()
-const mongoose = require('mongoose')
-const cors = require('cors')
-const router = require('./router')
-const morgan = require('morgan')
-const removeNonExistingClasses = require('./scripts/removeNonExistingClasses')
+const express = require('express');
+const config = require('config');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = require('./router');
+const morgan = require('morgan');
+const removeNonExistingClasses = require('./scripts/removeNonExistingClasses');
 
-const PORT = process.env.PORT || config.get('port') || 5000
-const mongoDbUrl = config.get('mongoDbUrl')
+require('dotenv').config();
 
-app.use(cors())
-app.use(morgan('tiny'))
-app.use(router)
+const PORT = process.env.PORT || config.get('port') || 5000;
+// const mongoDbUrl = config.get('mongoDbUrl');
+const mongoDbUrl = process.env.MONGODB_URL;
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(router);
 
 mongoose
   .connect(mongoDbUrl)
   .then(() => {
-    console.log('Connected to Mongoose')
+    console.log('Connected to Mongoose');
     app.listen(PORT, () => {
-      console.log(`server listening on port ${PORT}`)
-    })
+      console.log(`server listening on port ${PORT}`);
+    });
   })
-  .catch(e => console.log('Error in MONGOOSE CONNECT: ', e))
+  .catch(e => console.log('Error in MONGOOSE CONNECT: ', e));
 
 // app.listen(PORT, () => {
 //   console.log(`server listening on port ${PORT}`)
